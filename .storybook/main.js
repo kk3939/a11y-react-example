a370b79d9766b2f77e68905597739da5488579af
kk3939 <kyo51310@gmail.com>
2022-09-01 18:18:43 +0900

    setup:  storybook

---
+++ .storybook/main.js
@@ -0,0 +1,13 @@
+module.exports = {
+  "stories": [
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "storybook-readme",
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    "@storybook/addon-a11y"
+  ],
+  "framework": "@storybook/react"
+}
