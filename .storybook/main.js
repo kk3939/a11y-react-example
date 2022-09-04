09ab9f18e370a9a7d159fec036975e3eacf5b6b4
kyo9bo <45879786+kk3939@users.noreply.github.com>
2022-09-04 14:52:50 +0900

    update: webpack (#31)

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,13 +1,16 @@
 module.exports = {
-  "stories": [
+  stories: [
     "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
-  "addons": [
+  addons: [
     "storybook-readme",
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
     "@storybook/addon-a11y"
   ],
-  "framework": "@storybook/react"
+  framework: "@storybook/react",
+  core: {
+    builder: 'webpack5',
+  },
 }
