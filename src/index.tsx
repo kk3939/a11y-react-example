31a57658211b53c9bbdfa9c248a0df78ef51956a
kk3939 <kyo51310@gmail.com>
2022-09-10 14:03:40 +0900

    add: import and export

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,6 @@
 import { Accordion } from "./components/Accordion";
+import { Alert } from "./components/Alert";
+import { AlertDialog } from "./components/AlertDialog";
+import { Breadcrumb } from "./components/Breadcrumb";
 
-export { Accordion };
+export { Accordion, Alert, AlertDialog, Breadcrumb };
